6a6052ba98c2ad128525ce738bec00673b5b50bc
golden-on <70214821+golden-on@users.noreply.github.com>
2024-12-12 16:35:54 +0100

    tailwind config

---
+++ barbershop/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: 'rgb(157, 149, 112)',
+      },
+    },
+  },
+  plugins: [],
+}
